da4c5518d05c5e9e8c5c69c6b0a79d246f059934
Muhammad Muazzain <90068510+MuhammadMuazzain@users.noreply.github.com>
2025-09-12 00:15:03 +0500

    update

--- format.py
+++ format.py
@@ -53,5 +53,8 @@ def format_contacts():
 # if __name__ == "__main__":
 #     format_contacts()
 
+# if __name__ == "__main__":
+#     format_contacts()
+
 if __name__ == "__main__":
     format_contacts()
