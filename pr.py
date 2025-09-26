03603481f3a2a733aef5bc0ff3de86e178543993
Muhammad Muazzain <90068510+MuhammadMuazzain@users.noreply.github.com>
2025-09-26 23:43:22 +0500

    Mine

---
+++ pr.py
@@ -0,0 +1,35 @@
+fms='radar'
+
+left=0
+right=len(fms)-1
+
+
+
+# def palindrome_check(fms):
+#     while left < right:
+#         if fms[left] != fms[right]:
+#             return False
+#         left+=1
+#         right-=1
+#     return True
+
+
+# #Three sum solution(Changed/Updated)
+
+# def palindrome_check(fms):
+#     while left < right:
+#         if fms[left] != fms[right]:
+#             return False
+#         left+=1
+#         right-=1
+#     return True
+
+
+
+m1="s-i-p"
+c1 = m1.split('-')
+# print(c1)
+
+m2=['d', 'i', "m"]
+z1 = m2.join('')
+print(z1)
