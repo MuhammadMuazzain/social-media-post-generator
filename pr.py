65fc91010973402a7601e3ec6e8c674f477daeb7
Muhammad Muazzain <90068510+MuhammadMuazzain@users.noreply.github.com>
2025-10-02 20:16:15 +0500

    Fianl

--- pr.py
+++ pr.py
@@ -36,3 +36,15 @@ print(z1)
 m2=['d', 'i', "m"]
 z1 = m2.join('')
 print(z1)
+
+
+m2=['d', 'i', "m"]
+z1 = m2.join('')
+print(z1)
+m2=['d', 'i', "m"]
+z1 = m2.join('')
+print(z1)
+
+m2=['d', 'i', "m"]
+z1 = m2.join('')
+print(z1)
